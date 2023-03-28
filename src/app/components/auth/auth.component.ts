21d4ea9ce6bf2ab6e1dce9968443382cbec495dc
Victoria <victoria.golomazova@yandex.ru>
2023-03-28 14:46:41 +0300

    layout auth/register page completed

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageTypes } from 'src/app/services/types';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  public typePage!: PageTypes;
+
+  ngOnInit(): void {
+    this.typePage = 'register';
+  }
+
+  public toggleType(type: PageTypes): PageTypes {
+    return this.typePage = type;
+  }
 
 }
---
+++ src/app/services/types.ts
@@ -0,0 +1,1 @@
+export type PageTypes = 'auth' | 'register';
